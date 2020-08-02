72b515fbe5d25f3438bf7e4a7740b17a5a514c38
Stas Borisov <driupkin@mail.ru>
2020-08-02 17:02:47 +0300

    change menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import './index.css';
 const menuButton = document.querySelector('.menu__button');
+const menuContainer = document.querySelector('.menu__container');
 const menu = document.querySelector('.menu');
 menuButton.addEventListener('click', () => {
-    menu.classList.toggle('menu_open');
-    menuButton.classList.toggle('menu__button_active');
+    menuContainer.classList.toggle('menu__container_opened');
+    menu.classList.toggle('menu_opened');
 });
