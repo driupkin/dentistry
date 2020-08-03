d72fd7cf6cf1f15dc816bd896df3f0e77c963928
Stas Borisov <driupkin@mail.ru>
2020-08-03 03:03:55 +0300

    add header

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,15 @@ import './index.css';
 const menuButton = document.querySelector('.menu__button');
 const menuContainer = document.querySelector('.menu__container');
 const menu = document.querySelector('.menu');
+const page = document.querySelector('.page');
+
 menuButton.addEventListener('click', () => {
     menuContainer.classList.toggle('menu__container_opened');
     menu.classList.toggle('menu_opened');
+});
+document.addEventListener('click', (evt) => {
+    if (evt.target == page) {
+        menuContainer.classList.remove('menu__container_opened');
+        menu.classList.remove('menu_opened');
+    }
 });
