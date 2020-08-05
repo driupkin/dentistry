c6c706a627c199717f684516fcf41a8ed4e0c834
Stas Borisov <driupkin@mail.ru>
2020-08-05 03:14:06 +0300

    add logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,15 @@ const menuButton = document.querySelector('.menu__button');
 const menuContainer = document.querySelector('.menu__container');
 const menu = document.querySelector('.menu');
 const page = document.querySelector('.page');
+const logoHidden = menuContainer.querySelector('.logo');
 
 menuButton.addEventListener('click', () => {
     menuContainer.classList.toggle('menu__container_opened');
     menu.classList.toggle('menu_opened');
+    menuButton.classList.toggle('menu__button_active');
+    setTimeout(() => {
+        logoHidden.classList.toggle('logo_hidden')
+    }, 1000);
 });
 document.addEventListener('click', (evt) => {
     if (evt.target == page) {
