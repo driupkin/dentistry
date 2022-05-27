6f8bbd4fa4c709d69eac109e774221fec67500a1
Stas Borisov <driupkin@mail.ru>
2022-05-27 13:12:17 +0300

    new menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ menuButton.addEventListener('click', () => {
     menuContainer.classList.toggle('menu__container_opened');
     menu.classList.toggle('menu_opened');
     menuButton.classList.toggle('menu__button_active');
+    menuButton.classList.toggle('menu__button_rotation');
     setTimeout(() => {
         logoHidden.classList.toggle('logo_hidden')
     }, 1000);
@@ -17,6 +18,7 @@ document.addEventListener('click', (evt) => {
     if (evt.target == page) {
         menuContainer.classList.remove('menu__container_opened');
         menu.classList.remove('menu_opened');
+        menuButton.classList.add('menu__button_active');
     }
 });
 menuButton.addEventListener('mouseover', () => {
